2da7881f2b6cfe89470ce0432d2cc896f0a45c6b
Krupa <krupa.john@bds.christuniversity.in>
2024-04-21 23:37:12 +0530

    commit

--- todoproject/todoapp/urls.py
+++ todoproject/todoapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('todo/', views.todoappView, name='todo'),
     path('addTodoItem/',views.addTodoView), 
+    path('todo/edit/<int:id>/', views.editTodoView, name='edit_todo'),
     path('deleteTodoItem/<int:id>/', views.deleteTodoView), 
 ] 
